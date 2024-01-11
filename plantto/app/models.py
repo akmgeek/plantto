1a2d2a2b1761afc19acd01c92c92ec7d9bc4b38a
Amit Maurya <akmgeek@gmail.com>
2024-01-12 00:20:59 +0530

    fixed css

--- plantto/app/models.py
+++ plantto/app/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='app/static/product_images/')
+    image = models.ImageField(upload_to='product_images/')
     # Add other fields as needed
 
     def __str__(self):
